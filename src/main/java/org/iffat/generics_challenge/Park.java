e9f690125bf23597fd4ab356947b2f426f0e5fc1
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-03-24 22:08:34 +0700

    06. Generic Class Challenge Part 2

---
+++ src/main/java/org/iffat/generics_challenge/Park.java
@@ -0,0 +1,15 @@
+package org.iffat.generics_challenge;
+
+public class Park extends Point{
+    private String name;
+
+    public Park(String name, String location) {
+        super(location);
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " National Park";
+    }
+}
